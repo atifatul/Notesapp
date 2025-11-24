1ef4e878bbfc6d7f5b204bc8edd2c68b772c6dd9
Atif <atifatul752@gmail.com>
2025-11-25 01:30:29 +0530

    connected frontend to backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import axios from "axios"; // 1. Axios Import kiya
 
+// 2. Base URL set kiya (Apna Render wala URL yahan paste karo)
+// Note: Last mein slash '/' mat lagana
+axios.defaults.baseURL = "https://notesapp-atif-reyyani.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
- 
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
